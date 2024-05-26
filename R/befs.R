460728622a5ee8edbc99e50cc9c53ad104d29fcf
Christopher Kenny <ctk44@cornell.edu>
2024-05-26 14:38:02 -0400

    Create befs.R

---
+++ R/befs.R
@@ -0,0 +1,10 @@
+
+befs <- c(
+  # 109 - 113 are identical
+  cd_109 = 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2013/113-congressional-district-bef/cd113.zip',
+  cd_114 = 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2015/114-congressional-district-bef/cd114.zip',
+  cd_115 = 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2017/115-congressional-district-bef/cd115.zip',
+  cd_116 = 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2019/116-congressional-district-bef/cd116.zip',
+  # 2020 baf - 117
+  cd_118 = 'https://www2.census.gov/programs-surveys/decennial/rdo/mapping-files/2023/118-congressional-district-bef/cd118.zip'
+)
