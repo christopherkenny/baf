4536c5906ff8b099ef37dd8ef96ddf65e9073bbd
Christopher Kenny <ctk44@cornell.edu>
2024-05-27 16:24:04 -0400

    start baf fn

--- R/baf.R
+++ R/baf.R
@@ -1,3 +1,16 @@
-baf <- function(state, year, geography) {
+#' Title
+#'
+#' @param state
+#' @param year
+#' @param geographies
+#' @param cache_to
+#' @param refresh
+#'
+#' @return
+#' @export
+#'
+#' @examples
+baf <- function(state, year = 2020, geographies = NULL,
+                cache_to = baf_download_path(), refresh = FALSE) {
 
 }
--- R/baf_download.R
+++ R/baf_download.R
@@ -1,39 +1,47 @@
 #' Download a file
 #'
-#' Backend-agnostic (currently `curl`)
-#'
 #' @param url a URL
 #' @param path a file path
 #' @param overwrite should the file at path be overwritten if it already exists? Default is FALSE.
 #' @param quiet should output and progress bar be suppressed?
 #'
-#' @return the `curl` request
+#' @return a path to the downloaded file, invisibly
 #'
-#' @noRd
+#' @examples
+#' tf <- tempfile(fileext = '.zip')
+#' ex <- 'https://www2.census.gov/geo/docs/maps-data/data/baf2020/BlockAssign_ST01_AL.zip'
+#' baf_download(url = ex, path = tf)
 baf_download <- function(url, path, overwrite = FALSE, quiet = FALSE) {
   dir <- dirname(path)
   if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
+
   if (!file.exists(path) || overwrite) {
     curl::curl_download(url, path, quiet = quiet)
   } else {
     cli::cli_inform(c('File already downloaded at {.path {path}}',
       '>' = 'Set {.arg overwrite = TRUE} to overwrite.'
     ))
-    list(status_code = 200)
   }
+
+  invisible(path)
 }
 
 
 
-#' Figure out where to download things
+
+#' Get download location for `baf` files
 #'
-#' @noRd
+#' @return a path
+#' @export
+#'
+#' @examples
+#' baf_download_path()
 baf_download_path <- function() {
   user_cache <- getOption('baf.cache_dir')
   if (!is.null(user_cache)) {
     p <- user_cache
   } else if (getOption('baf.use_cache', FALSE)) {
-    p <- tools::R_user_dir('maup', 'data')
+    p <- tools::R_user_dir('baf', 'data')
   } else {
     p <- tempdir()
   }
