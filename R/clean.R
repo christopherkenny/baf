e41f0f1d7c9f633d23de915a528eef0ed8d959a5
Christopher Kenny <ctk44@cornell.edu>
2024-05-31 14:47:54 -0400

    Update clean.R

--- R/clean.R
+++ R/clean.R
@@ -19,31 +19,66 @@ clean_bafs <- function(bafs, state = NULL) {
   lapply(noms, function(nom) {
     if (nom == 'AIANNH') {
       bafs[[nom]] |>
+        dplyr::rename_with(
+          .fn = function(x) sub(pattern = 'GEOID', replacement = 'BLOCKID', x = x),
+          .cols = dplyr::any_of('GEOID')
+        ) |>
         dplyr::mutate(
           GEOID = .data$BLOCKID,
-          target = dplyr::if_else(is.na(.data$AIANNHCE), NA_character_, paste0(abb, .data$AIANNHCE)),
+          ai_an_nh = dplyr::if_else(is.na(.data$AIANNHCE), NA_character_, paste0(abb, .data$AIANNHCE)),
           .keep = 'none'
         )
     } else if (nom == 'INCPLACE_CDP') {
       bafs[[nom]] |>
+        dplyr::rename_with(
+          .fn = function(x) sub(pattern = 'GEOID', replacement = 'BLOCKID', x = x),
+          .cols = dplyr::any_of('GEOID')
+        ) |>
         dplyr::mutate(
           GEOID = .data$BLOCKID,
-          target = dplyr::if_else(is.na(.data$PLACEFP), NA_character_, paste0(abb, .data$PLACEFP)),
+          place = dplyr::if_else(is.na(.data$PLACEFP), NA_character_, paste0(abb, .data$PLACEFP)),
           .keep = 'none'
         )
-    } else if (nom %in% c('CD', 'SDUNI', 'SLDL', 'SLDU')) {
+    } else if (nom %in% c('CD', 'SDUNI', 'SLDL', 'SLDLST', 'SLDU', 'SLDUST')) {
+      short <- c(CD = 'cd', SDUNI = 'school', SLDL = 'shd', SLDU = 'ssd')[nom]
       bafs[[nom]] |>
+        dplyr::rename_with(
+          .fn = function(x) sub(pattern = 'GEOID', replacement = 'BLOCKID', x = x),
+          .cols = dplyr::any_of('GEOID')
+        ) |>
         dplyr::mutate(
           GEOID = .data$BLOCKID,
-          target = dplyr::if_else(is.na(.data$DISTRICT), NA_character_, paste0(abb, .data$DISTRICT)),
+          !!short := dplyr::if_else(is.na(.data$DISTRICT), NA_character_, paste0(abb, .data$DISTRICT)),
           .keep = 'none'
         )
     } else if (nom == 'VTD') {
       bafs[[nom]] |>
+        dplyr::rename_with(
+          .fn = function(x) sub(pattern = 'GEOID', replacement = 'BLOCKID', x = x),
+          .cols = dplyr::any_of('GEOID')
+        ) |>
         dplyr::mutate(
           GEOID = .data$BLOCKID,
-          target = paste0(abb, .data$COUNTYFP, .data$DISTRICT),
+          vtd = paste0(abb, .data$COUNTYFP, .data$DISTRICT),
           .keep = 'none'
+        )
+    } else {
+      bafs[[nom]] |>
+        dplyr::rename_with(
+          .fn = function(x) sub(pattern = 'BLOCKID', replacement = 'GEOID', x = x),
+          .cols = dplyr::any_of('BLOCKID')
+        ) |>
+        dplyr::rename_with(
+          .fn = function(x) 'cd'[seq_along(x)],
+          .cols = dplyr::any_of('CDFP')
+        ) |>
+        dplyr::rename_with(
+          .fn = function(x) 'shd'[seq_along(x)],
+          .cols = dplyr::any_of('SLDLST')
+        ) |>
+        dplyr::rename_with(
+          .fn = function(x) 'ssd'[seq_along(x)],
+          .cols = dplyr::any_of('SLDUST')
         )
     }
   }) |>
